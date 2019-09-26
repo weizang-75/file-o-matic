de3dc03a36c6cb33987724908cac9cbddaabea56
deutscher-chris <christoph.dorward.1977@gmail.com>
2019-09-26 22:31:02 +1000

    Add a config file

---
+++ config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+    config
+    errr, yeh. just change this, really. It should **just work**
+*/
+
+exports.config = {
+    srcDir:`./svg`, // path to your directory of SVG's (relative to this config file)
+    outputDir:`./jsx`, // path the converted files are saved to
+};
